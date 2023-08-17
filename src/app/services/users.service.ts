96f1253ed3ee9273b9006a0a20ff5d9622163622
CaroBima <carobima@gmail.com>
2023-08-17 18:13:53 -0300

    Agregando menu al header y inputs en componente de registro

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+  constructor(private http: HttpClient) {}
+
+  login(user: any): Observable<any> {
+    return this.http.post("https://reqres.in/api/login", user);
+  }
+  
+}
