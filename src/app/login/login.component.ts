c879487faf1d424db3f19686fc5ff8a092c29a23
CaroBima <carobima@gmail.com>
2023-09-03 10:50:49 -0300

    Agregando servicios y modelos para el manejo de la sesion

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginUsuario } from '../models/login-usuario';
+import { AuthService } from '../services/auth.service';
+import { TokenService } from '../services/token.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  form: any = {};
+  usuario: LoginUsuario | undefined; 
+  isLogged = false;
+  isLoginFail = false;
+  roles: string[] = [];
+  errorMsg = '';
+
+  constructor(private authService: AuthService, private tokenService: TokenService, private router: Router) { }
+
+  ngOnInit() {
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+      this.isLoginFail = false;
+      this.roles = this.tokenService.getAuthorities();
+    }
+  }
+
+  onLogin(): void {
+    this.usuario = new LoginUsuario(this.form.nombreUsuario, this.form.password);
+
+    this.authService.login(this.usuario).subscribe(data => {
+      this.tokenService.setToken(data.token);
+      this.tokenService.setUserName(data.nombreUsuario);
+      this.tokenService.setAuthorities(data.authorities);
+
+      this.isLogged = true;
+      this.isLoginFail = false;
+      this.roles = this.tokenService.getAuthorities();
+      window.location.reload();
+    },
+      (err: any) => {
+        this.isLogged = false;
+        this.isLoginFail = true;
+        this.errorMsg = err.error.message;
+      }
+    );
+  }
+
+}
+/*
+import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../services/users.service';
 import { Router } from '@angular/router';
 
@@ -30,3 +83,4 @@ export class LoginComponent implements OnInit {
 
   
 }
+*/
---
+++ src/app/services/token.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+
+const TOKEN_KEY = 'AuthToken';
+const USERNAME_KEY = 'AuthUserName';
+const AUTHORITIES_KEY = 'AutAuthorities';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  roles: Array<string> = [];
+
+  constructor() { }
+
+  public setToken(token: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+  }
+
+  public getToken(): string {
+    const token = sessionStorage.getItem(TOKEN_KEY);
+    return token !== null ? token : ''; // Devuelve una cadena vacía si el token es nulo.
+  }
+  
+
+  public setUserName(userName: string): void {
+    window.sessionStorage.removeItem(USERNAME_KEY);
+    window.sessionStorage.setItem(USERNAME_KEY, userName);
+  }
+
+  public getUserName(): string {
+    const sessionStore = sessionStorage.getItem(USERNAME_KEY);
+    return sessionStore !== null ? sessionStore : '';
+  }
+
+  public setAuthorities(authorities: string[]): void {
+    window.sessionStorage.removeItem(AUTHORITIES_KEY);
+    window.sessionStorage.setItem(AUTHORITIES_KEY, JSON.stringify(authorities));
+  }
+
+  public getAuthorities(): string[] {
+    this.roles = [];
+    if (sessionStorage.getItem(AUTHORITIES_KEY)) {
+      JSON.parse(sessionStorage.getItem(AUTHORITIES_KEY)!).forEach((authority: { authority: string; }) => {
+        this.roles.push(authority.authority);
+      });
+    }
+    return this.roles;
+  }
+
+  public logOut(): void {
+    window.sessionStorage.clear();
+  }
+}
